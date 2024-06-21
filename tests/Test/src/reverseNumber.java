94e10250184886039246bc8f8c1bdd04d5e06fe7
Mohammed Said <ms22abuabdelrahman@gmail.com>
2024-06-21 12:10:43 +0200

    reverse reverseNumber

---
+++ tests/Test/src/reverseNumber.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class reverseNumber {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter a number: ");
+    int num = input.nextInt();
+    int rev = 0;
+    while (num > 0) {
+      rev = rev * 10 + num % 10;
+      num = num / 10;
+    }
+    System.out.println(rev);
+  }
+  
+}
